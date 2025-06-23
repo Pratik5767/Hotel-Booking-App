224f809c739681f651111f03bc6cc732c5afa10d
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-06-23 12:42:34 +0530

    created the entity and response dto's

--- lakeside-hotel-backend/src/main/java/com/project/lakeside_hotel_backend/model/BookedRoom.java
+++ lakeside-hotel-backend/src/main/java/com/project/lakeside_hotel_backend/model/BookedRoom.java
@@ -1,18 +1,50 @@
 package com.project.lakeside_hotel_backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookingId;
+
+    @Column(name = "check_In")
     private LocalDate checkInDate;
+
+    @Column(name = "check_Out")
     private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
     private String guestFullName;
+
+    @Column(name = "guest_Email")
     private String guestEmail;
+
+    @Column(name = "children")
     private int numOfChildren;
+
+    @Column(name = "adults")
     private int numOfAdults;
+
+    @Column(name = "total_guest")
     private int totalNumOfGuest;
+
+    @Column(name = "confirmation_code")
     private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
     private Room room;
 
     public void calculateTotalNumberOfGuests() {
@@ -29,7 +61,7 @@ public class BookedRoom {
         calculateTotalNumberOfGuests();
     }
 
-//    public void setBookingConfirmationCode(String bookingConfirmationCode) {
-//        this.bookingConfirmationCode = bookingConfirmationCode;
-//    }
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
