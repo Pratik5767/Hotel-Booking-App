515d01c050f9b03c9b1a9b0a91f2574d2e2ea6fe
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-06-27 18:59:19 +0530

    created navbar and footer component and Browse all room component with filtering functionality

--- lakeside-hotel-frontend/src/App.jsx
+++ lakeside-hotel-frontend/src/App.jsx
@@ -8,20 +8,24 @@ import EditRoom from './components/room/EditRoom.jsx'
 import AddRoom from './components/room/AddRoom.jsx'
 import Navbar from './components/layout/Navbar.jsx'
 import Footer from './components/layout/Footer.jsx'
+import RoomListing from './components/room/RoomListing.jsx'
+import Admin from './components/admin/Admin.jsx'
 
 const App = () => {
     return (
         <>
-            <Navbar/>
+            <Navbar />
 
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/edit-room/:roomId' element={<EditRoom />} />
                 <Route path='/existing-rooms' element={<ExistingRooms />} />
                 <Route path='/add-room' element={<AddRoom />} />
+                <Route path='/browse-all-rooms' element={<RoomListing />} />
+                <Route path='/admin' element={<Admin />} />
             </Routes>
 
-            <Footer/>
+            <Footer />
         </>
     )
 }
