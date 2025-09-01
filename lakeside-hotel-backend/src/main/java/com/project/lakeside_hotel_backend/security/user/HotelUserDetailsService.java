586c132152c8661f0aa00ff4298023850763f0cd
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-09-01 16:20:30 +0530

    Addded the security configurations

---
+++ lakeside-hotel-backend/src/main/java/com/project/lakeside_hotel_backend/security/user/HotelUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.project.lakeside_hotel_backend.security.user;
+
+import com.project.lakeside_hotel_backend.model.User;
+import com.project.lakeside_hotel_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return HotelUserDetails.buildUserDetails(user);
+    }
+}
